c2f343912abd15b573b59163bea6815b44bc7753
husen.telwala <husen.telwala@autofacets.in>
2025-10-03 16:13:57 +0530

    Add transaction changes

--- src/app/expense-manager/add-transaction/add-transaction.component.ts
+++ src/app/expense-manager/add-transaction/add-transaction.component.ts
@@ -2,27 +2,37 @@ import { Component } from '@angular/core';
 import { Observable, Subscription, take } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { addDoc, collection, collectionData, doc, Firestore, updateDoc } from '@angular/fire/firestore';
-import { Category, TransactionLog } from '../../modal/transaction.modal';
+import { CategoryModal, TransactionLogModal } from '../../modal/transaction.modal';
 import { TransactionService } from '../../service/transaction.service';
 import { ActivatedRoute, Route, Router, RouterModule } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core'; 
 
 @Component({
   selector: 'app-add-transaction',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    RouterModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule
+  ],
   templateUrl: './add-transaction.component.html',
   styleUrls: ['./add-transaction.component.scss']
 })
 export class AddTransactionComponent {
   transactionForm!: FormGroup;
-  allTransactions$!: Observable<TransactionLog[]>;
-  categories$!: Observable<Category[]>;
+  allTransactions$!: Observable<TransactionLogModal[]>;
+  categories$!: Observable<CategoryModal[]>;
   transactionId: string | null = null;
   collectionTransaction = 'TransactionLog';
   subscriptions: Subscription[] = [];
 
-  constructor(private activatedRoute: ActivatedRoute, private router: Router, private fb: FormBuilder, private firestore: Firestore, private transactionService: TransactionService) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private fb: FormBuilder, private transactionService: TransactionService) {}
 
   ngOnInit() {
     this.transactionId = this.activatedRoute.snapshot.paramMap.get('id');
@@ -37,7 +47,14 @@ export class AddTransactionComponent {
   async loadTransaction(id: string) {    
     const snapshot = await this.transactionService.getRecordById(this.collectionTransaction, id);
     if (snapshot.exists()) {
-      this.transactionForm.patchValue(snapshot.data() as TransactionLog);
+      const data =  snapshot.data() as  TransactionLogModal;
+      const transactionDate = this.transactionService.formatDate(data.date);
+      const dataWithFormattedDate = {
+        ...data,
+        date: new Date(transactionDate),
+      }
+      console.log(dataWithFormattedDate);
+      this.transactionForm.patchValue(dataWithFormattedDate);
     }
   }
 
@@ -46,20 +63,22 @@ export class AddTransactionComponent {
       cause: this.fb.control('', { nonNullable: true, validators: [Validators.required]}),
       amount: this.fb.control(0, { nonNullable: true, validators: [Validators.required]}),
       categoryId: this.fb.control('', { nonNullable: true, validators: [Validators.required]}),
-      transactionType: this.fb.control(false, {nonNullable: true})
+      transactionType: this.fb.control(false, {nonNullable: true}),
+      date: this.fb.control ((new Date()), { nonNullable: true, validators: [Validators.required]})
     });
+    console.log(this.transactionForm.value.date);
+
   }
 
   onSubmit(): void {    
     console.log('Transaction form submitted');   
     if (this.transactionForm.valid) {      
-      const transactionLog: TransactionLog = this.transactionForm.getRawValue();
+      const transactionLog: TransactionLogModal = this.transactionForm.getRawValue();
       this.subscriptions.push(this.categories$.subscribe(categories => {              
         const selectedCat = categories.find(c => c.categoryId === transactionLog.categoryId);
-        const data: TransactionLog = {
+        const data: TransactionLogModal = {
             ...transactionLog,
-            categoryName: selectedCat ? selectedCat.name : '',
-            date: new Date()            
+            categoryName: selectedCat ? selectedCat.name : ''
         }
         if(this.transactionId) { // EDIT
           this.transactionService.updateRecord(this.collectionTransaction, this.transactionId, data);
