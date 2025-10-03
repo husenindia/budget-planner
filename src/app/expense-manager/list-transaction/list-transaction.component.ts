a87884d0d5817c185240ac408c1a3035be9a4469
husen.telwala <husen.telwala@autofacets.in>
2025-10-03 16:13:25 +0530

    Transaction list changes

--- src/app/expense-manager/list-transaction/list-transaction.component.ts
+++ src/app/expense-manager/list-transaction/list-transaction.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { map, Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { TransactionLogWithFormattedDate } from '../../modal/transaction.modal';
 import { TransactionService } from '../../service/transaction.service';
 import { RouterModule } from '@angular/router';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { ConfirmationPopupComponent } from '../../common/confirmation-popup/confirmation-popup.component';
 
 
 @Component({
   selector: 'app-list-transaction',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatDialogModule],
   templateUrl: './list-transaction.component.html',
   styleUrl: './list-transaction.component.scss'
 })
@@ -17,9 +19,13 @@ export class ListTransactionComponent {
   allTransactions$!: Observable<TransactionLogWithFormattedDate[]>;
   collectionName = 'TransactionLog';
   collectionId  = 'transactionId';
+  sortedColumnName = 'date';
+  sortedDirection: 'asc' | 'desc' = 'desc';
+  subscriptions: Subscription[] = [];
+
   constructor(private transactionService: TransactionService) {}
   ngOnInit() {
-    this.allTransactions$ = this.transactionService.getAllRecords(this.collectionName, this.collectionId).pipe(
+    this.allTransactions$ = this.transactionService.getAllRecords(this.collectionName, this.collectionId, this.sortedColumnName, this.sortedDirection).pipe(
       map(transactions=> 
         transactions.map(transaction => ({
           ...transaction,
@@ -28,8 +34,22 @@ export class ListTransactionComponent {
       )
     );
   }  
-  deleteRecord(id:string) {
-    this.transactionService.deleteRecord(this.collectionName, id);
+  readonly dialog = inject(MatDialog);
+  deleteConfirmation(transactionId:string) {
+    const dialogRef = this.dialog.open(ConfirmationPopupComponent);
+    this.subscriptions.push(dialogRef.afterClosed().subscribe(result => {
+      if(result==true) {
+        this.transactionService.deleteRecord(this.collectionName, transactionId);
+      }
+      else {
+        console.log('Delete cancelled');
+      }
+    }));
   }
   
+  ngOnDestroy(): void {
+    if(this.subscriptions.length>0) {
+      this.subscriptions.forEach((sub)=>sub.unsubscribe());
+    }
+  }
 }
